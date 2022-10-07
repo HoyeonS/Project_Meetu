923bd432c1440bee97fff71a2481f70f5cdd0750
Jaesung411 <dlawo411@gmail.com>
2022-10-07 18:37:52 -0500

    add status and friend activity

--- app/src/main/java/com/example/project_meetu/StatusActivity.java
+++ app/src/main/java/com/example/project_meetu/StatusActivity.java
@@ -7,25 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class StatusActivity extends AppCompatActivity {
 
     private Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_status);
 
-        btn_next = findViewById(R.id.btn_main);
-
+        btn_next = findViewById(R.id.btn_next);
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,StatusActivity.class);
+                Intent intent = new Intent(StatusActivity.this,FriendActivity.class);
                 startActivity(intent);
             }
         });
     }
-
-
 }
