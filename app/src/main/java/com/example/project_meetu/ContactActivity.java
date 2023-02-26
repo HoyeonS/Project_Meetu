fbd2d78c233285a7b2a677b05b3291885a84db37
Jaesung411 <dlawo411@gmail.com>
2023-02-25 18:52:53 -0600

    add the parameter for server in retrofit

--- app/src/main/java/com/example/project_meetu/ContactActivity.java
+++ app/src/main/java/com/example/project_meetu/ContactActivity.java
@@ -6,28 +6,93 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 
 public class ContactActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+
         Button BtnContactActivity = findViewById(R.id.btn_contact_activity);
+        final EditText introEdit = findViewById(R.id.intro);
+        final EditText contactEdit = findViewById(R.id.contact);
+
         BtnContactActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ContactActivity.this,StatusActivity.class);
-                startActivity(intent);
+
                 /**
                  * Retrofit interface @POST("./signup)
                  * INPUT: Student information from instance
                  * OUTPUT: Validality
                  */
-                // HelloWorld
+                HashMap<String, String> map = new HashMap<>();
 
+                map.put("name",Student.getInstance().getName());
+                map.put("age",Student.getInstance().getProfile()[0]);
+                map.put("major",Student.getInstance().getProfile()[2]);
+                map.put("place",Student.getInstance().getProfile()[3]);
+                map.put("fb1",Student.getInstance().getProfile()[4]);
+                map.put("fb2",Student.getInstance().getProfile()[5]);
+                map.put("food",Student.getInstance().getProfile()[6]);
+                map.put("lang",Student.getInstance().getProfile()[7]);
+                map.put("hobby",Student.getInstance().getProfile()[8]);
+                map.put("intro",introEdit.getText().toString());
+                map.put("contact",contactEdit.getText().toString());
+
+                Call<Void> call = retrofitInterface.executeSignup(map);
+
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        //성공시
+                        if(response.code() == 200){
+                            startActivity(intent);
+                        }else if(response.code() == 404){
+                            Toast.makeText(ContactActivity.this, "Wrong Credentials",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        //실패시 알림
+                        Toast.makeText(ContactActivity.this, t.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
+
     }
+
+    Call<Void> executeSignup(@Body HashMap<String, String> map){
+        return null; //need-to-be-changed
+    }
+
 }
--- app/src/main/java/com/example/project_meetu/LoginActivity.java
+++ app/src/main/java/com/example/project_meetu/LoginActivity.java
@@ -7,19 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
 
         Button LoginBtn = findViewById(R.id.LoginBtn);
 
@@ -40,14 +55,38 @@ public class LoginActivity extends AppCompatActivity {
                  * Signup signal up with user iD
                  */
                 HashMap<String, String> map = new HashMap<>();
-                Call<Student> call = executeLogin(map);
+                //Call<Student> call = executeLogin(map);
                 map.put("student_id", UserID.getInstance().getUserId());//wisconsin ID with signal > output : student object
-                startActivity(intent);
+
+                Call<Student> call = retrofitInterface.executeLogin(map);
+
+                call.enqueue(new Callback<Student>() {
+                    @Override
+                    public void onResponse(Call<Student> call, Response<Student> response) {
+                        //성공시 알림
+                        if(response.code() == 200){
+                            startActivity(intent);
+                        }else if(response.code() == 404){
+                            Toast.makeText(LoginActivity.this, "Wrong Credentials",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Student> call, Throwable t) {
+                        //실패시 알림
+                        Toast.makeText(LoginActivity.this, t.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
             }
         });
     }
 
     Call<Student> executeLogin(@Body HashMap<String, String> map){
+        //id를 가지고 어떻게 student를 가져오조?
+        //Student student =
         return null; //need-to-be-changed
     }
 }
